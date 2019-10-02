5543edb29233dd4e88d9c8e7fb0a6c2fd7d0c531
pcunnell <philip.cunnell@clickfox.com>
2019-10-02 08:28:47 -0600

    setting up basic validations

--- 04-auth-from-scratch/client/src/app/signup/signup.component.ts
+++ 04-auth-from-scratch/client/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    password: new FormControl('', [Validators.minLength(12), Validators.required]),
+    confirmPassword: new FormControl('', [Validators.minLength(12), Validators.required]),
+  }, this.passwordMatchValidator);
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngOnChanges() {
+
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.get('confirmPassword').errors)
+    console.log(this.signupForm.getRawValue())
+    // if (this.validUser()) {
+    //   // send data to server
+    // }
+  }
+
+  // validUser() {
+
+  // }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value
+       ? null : {'mismatch': true};
   }
 
 }
