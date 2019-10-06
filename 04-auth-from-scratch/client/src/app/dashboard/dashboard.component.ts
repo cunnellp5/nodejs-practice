af2d0dabb9bddcc605347a306180fa29ef682765
pcunnell <philip.cunnell@clickfox.com>
2019-10-06 12:10:48 -0600

    created new route for user notes

--- 04-auth-from-scratch/client/src/app/dashboard/dashboard.component.ts
+++ 04-auth-from-scratch/client/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,8 +9,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  API_URL = 'http://localhost:5000';
+  API_URL = 'http://localhost:5000/';
   user = null;
+  showForm = true;
+  
+  noteForm = new FormGroup({
+    title: new FormControl(''),
+    note: new FormControl('')
+  });
+
   constructor( private router: Router ) { }
 
   ngOnInit() {
@@ -30,6 +39,24 @@ export class DashboardComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
+  }
+
+  addNote(e) {
+    e.preventDefault();
+    console.log(this.noteForm.getRawValue(),'noteform!')
+    fetch(`${this.API_URL}api/v1/notes`, {
+        method: 'post',
+        body: JSON.stringify(this.noteForm.getRawValue()),
+        headers: {
+          'content-type': 'application/json',
+          authorization: `Bearer ${localStorage.token}`,
+        }
+    })
+    .then(res => res.json())
+    .then((note) => {
+        console.log(note)
+    })
+
   }
 
 }
