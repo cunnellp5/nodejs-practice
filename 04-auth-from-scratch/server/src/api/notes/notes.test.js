2293541538d5ac3aa23f21fd2743db6e6c9e7049
pcunnell <philip.cunnell@clickfox.com>
2019-10-16 08:05:13 -0600

    added another test for getting notes

--- 04-auth-from-scratch/server/src/api/notes/notes.test.js
+++ 04-auth-from-scratch/server/src/api/notes/notes.test.js
@@ -9,7 +9,7 @@ const notes = db.get('notes');
 
 const newUser = {
   username: 'testuser',
-  password: '12345678900',
+  password: '1234567890',
 };
 
 const note = {
@@ -33,7 +33,34 @@ describe('GET notes /api/v1/notes', () => {
     expect(response.body.message).to.equal('🚫 Un-Authorized 🚫');
   });
 
+  it('should respond with all notes created by the user', async () => {
+    await users.remove({});
 
+    const signup = await request(app)
+      .post('/auth/signup')
+      .send(newUser)
+      .expect(200);
+    
+    expect(signup.body).to.have.property('token');
+    const token = signup.body.token;
+
+    const createNote = await request(app)
+      .post('/api/v1/notes')
+      .send(note)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+    
+    const getNotes = await request(app)
+      .get('/api/v1/notes')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+    
+    expect(getNotes.body[0].title).to.equal('hello')
+    expect(getNotes.body[0].note).to.equal('world')
+  })
+})
+
+describe('POST notes /api/v1/notes', () => {
   it('should create a note if a user is signed in', async () => {
     await users.remove({});
 
